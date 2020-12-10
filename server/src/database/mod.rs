ed1c3e334e4ea8efd9030e2dfb9269a1ee9c5927
Rampeo Mattone <46228228+RampeoMattone@users.noreply.github.com>
2020-12-10 12:54:58 +0000

    started refractoring, improved exception handling, better responses from server in case of query hit

--- server/src/database/mod.rs
+++ server/src/database/mod.rs
@@ -1,12 +1,50 @@
 use serde::Serialize;
-use sqlx::FromRow;
+use sqlx::{Pool, MySql, Error, Row, FromRow, Decode, Type, ColumnIndex};
+use std::collections::HashMap;
 
-#[derive(FromRow, Debug, Serialize)]
-pub struct Output(pub bool, pub String, pub String);
-/*
-pub struct Row{
-    pub success: bool,
-    pub code: String,
-    pub user: String
+#[derive(Serialize)]
+//pub struct Output(pub bool, pub String, pub String);
+pub struct Output{
+    success: bool,
+    code: String,
+    user: String
 }
-*/
+
+impl<'r, R> FromRow<'r, R> for Output where
+    R: Row,
+    bool: Decode<'r, <R as Row>::Database> + Type<<R as Row>::Database>,
+    String: Decode<'r, <R as Row>::Database> + Type<<R as Row>::Database>,
+    //&'r str: ColumnIndex<R>, // currently not working. possible issue with the api. for now only use column ordinals
+    usize: ColumnIndex<R> {
+    fn from_row(row: &'r R) -> Result<Self, Error> {
+        match (
+            row.try_get::<bool, _>(0),
+            row.try_get::<String, _>(1),
+            row.try_get::<String, _>(2))
+        {
+            (Ok(success), Ok(code), Ok(user)) => Ok(Output{success, code, user}),
+            (Err(e), _, _) | (_, Err(e), _) |(_, _, Err(e))  => {
+                //eprintln!("{:?}\n{:?}\n{:?}\n", success, code, user);
+                Err(e)
+            }
+        }
+    }
+}
+
+impl Output {
+    pub async fn new(body: HashMap<String, String>, pool: &Pool<MySql>) -> Result<Self, Error> {
+        match (body.get("user"), body.get("code")) { // using a match statement to make sure we get all the required JSON parameters
+            (Some(user), Some(code)) => { // if all the correct parameters are present we extract them and generate a proper response
+                sqlx::query_as::<_, Output>(
+                    //let result = sqlx::query(
+                    "call charitable.register( ? , ? )")
+                    .bind(code)
+                    .bind(user) // use an ad-hoc procedure to register the code to a user
+                    .fetch_one(pool).await // if the code cannot be registered, the procedure will return false, else it will return true
+            }
+            _ => {
+                Err(Error::ColumnNotFound(String::from("failed to find column 'user' or 'code'")))
+            }
+        }
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,61 +1,47 @@
 use sqlx::{mysql::{MySqlPoolOptions}};
 use warp::{Filter, http::StatusCode};
-use std::{collections::HashMap};
+use std::{env, collections::HashMap};
 
 mod database;
 use crate::database::Output;
 
 #[tokio::main]
 async fn main() {
-    // get a database url.
-    // TODO - IMPLEMENT ENVIRONMENT VARIABLES
-    let database_url = String::from("mysql://user:@pi:3306/charitable");
-    // generate a connection pool for the database
-    let pool = MySqlPoolOptions::new()
-        .max_connections(5)
-        .connect(&database_url).await
-        .expect("Failed to connect to the database");
-    // prepare the service:
-    // POST /reg/<code> => 200 OK, body "here is the code! <code>"
-    let service = warp::post() // listen only for POST requests
-        .and(warp::path!("reg")) // make sure the user is accessing /reg/<capture>
-        .and(warp::body::content_length_limit(4096)) // make sure the body is not too big
-        .and(warp::body::json())
-        .and_then(move |body: HashMap<String, String>| { // make a service that runs on an async function
-            // the pool is captured by the closure
-            let pool = pool.clone(); // the pool is cloned and passed with ownership to the async
-            async move {
-                match (body.get("user"), body.get("code")) { // using a match statement to make sure we get all the required JSON parameters
-                    (Some(user), Some(code)) => { // if all the correct parameters are present we extract them and generate a proper response
-                        let result: Output = sqlx::query_as(
-                        //let result = sqlx::query(
-                        "call charitable.register( ? , ? )")
-                            .bind(code)
-                            .bind(user) // use an ad-hoc procedure to register the code to a user
-                            .fetch_one(&pool).await.unwrap(); // if the code cannot be registered, the procedure will return false, else it will return true
-                        /*
-                        println!("{:?}", sqlx::Row::columns(&result));
-                        println!("{:?}", Output{
-                            success: false,
-                            code: "2".to_string(),
-                            user: "1".to_string()
-                        });
-                         */
-                        //let json = serde_json::to_string(&result).unwrap();
-                        if result.0 { // the first column of the result will always say whether or not we had a successful operation on the data
-                            //warp::reply::with_status(json, warp::http::StatusCode::OK)
-                            Ok(warp::reply::with_status(warp::reply::json(&result), StatusCode::OK))
-                        } else {
-                            //warp::reply::with_status("database said no".to_string(), warp::http::StatusCode::NOT_ACCEPTABLE)
-                            Ok(warp::reply::with_status(warp::reply::json(&result), StatusCode::CONFLICT))
+    // get a database url from an environment var
+    match env::var("DATABASE_URL") {
+        Ok(database_url) => {
+            // generate a connection pool for the database
+            let pool = MySqlPoolOptions::new()
+                .max_connections(5)
+                .connect(&database_url).await
+                .expect("Failed to connect to the database");
+            // prepare the service:
+            // POST /reg/<code> => 200 OK, body "here is the code! <code>"
+            let service = warp::post() // listen only for POST requests
+                .and(warp::path!("reg")) // make sure the user is accessing /reg/<capture>
+                .and(warp::body::content_length_limit(4096)) // make sure the body is not too big
+                .and(warp::body::json())
+                .and_then(move |body: HashMap<String, String>| { // make a service that runs on an async function
+                    // the pool is captured by the closure
+                    let pool = pool.clone(); // the pool is cloned and passed with ownership to the async
+                    async move {
+                        match Output::new(body, &pool).await { // using an ad hoc method to query the database and export the result idiomatically
+                            Ok(result) => // if the database call succeeded (if we received a row of values that we could convert from the query)
+                                    Ok(warp::reply::with_status(warp::reply::json(&result), StatusCode::OK)), // reply to the client with a json-formatted response that matches what we extracted from the database
+                            Err(e) => { // if the database call failed (either because of an improper query or because the parameter from the request body were missing)
+                                eprintln!("{:?}", e); // print the error we get to the error buffer
+                                Err(warp::reject::not_found()) // reply to the client with an empty 404
+                            }
                         }
-                    }// async block returns a response for the client
-                    _ => Err(warp::reject::not_found()) //warp::reply::with_status("fuck_off".to_string(), warp::http::StatusCode::NOT_FOUND) // if any of the two parameters are absent then we send a not found code and close it off like that
-                }
-            } // async block is returned as a future
-        });
+                    } // async block is returned as a future
+                });
 
-    warp::serve(service)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+            warp::serve(service)
+                .run(([127, 0, 0, 1], 3030))
+                .await;
+        },
+        Err(_) => {
+            eprintln!("please set DATABASE_URL to a mysql database url!")
+        },
+    };
 }
